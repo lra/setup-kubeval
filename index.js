04b31e991343872a4d6ca621a538dbc39263fdd9
Laurent Raufaste <analogue@glop.org>
2020-06-11 22:11:50 -0400

    Try with ncc

---
+++ index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+const options = {};
+options.cwd = '/tmp/';
+
+const version = core.getInput('version');
+
+async function main() {
+  try {
+    await exec.exec('curl', [
+      '-L',
+      '-o /tmp/kubeval-linux-amd64.tar.gz',
+      `https://github.com/instrumenta/kubeval/releases/download/${payload}/kubeval-linux-amd64.tar.gz`
+    ], options);
+    await exec.exec('tar', ['xf', 'kubeval-linux-amd64.tar.gz']);
+    await exec.exec('sudo', ['cp', 'kubeval', '/usr/local/bin']);
+    await exec.exec('rm', ['kubeval', 'kubeval-linux-amd64.tar.gz']);
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+main();
