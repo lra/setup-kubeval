b09c0796a547c5d7aef621ff93cd159d95d9a1d2
Laurent Raufaste <analogue@glop.org>
2020-06-11 22:15:26 -0400

    ::error::payload is not defined

--- dist/index.js
+++ dist/index.js
@@ -976,7 +976,7 @@ async function main() {
     await exec.exec('curl', [
       '-L',
       '-o /tmp/kubeval-linux-amd64.tar.gz',
-      `https://github.com/instrumenta/kubeval/releases/download/${payload}/kubeval-linux-amd64.tar.gz`
+      `https://github.com/instrumenta/kubeval/releases/download/${version}/kubeval-linux-amd64.tar.gz`
     ], options);
     await exec.exec('tar', ['xf', 'kubeval-linux-amd64.tar.gz']);
     await exec.exec('sudo', ['cp', 'kubeval', '/usr/local/bin']);
