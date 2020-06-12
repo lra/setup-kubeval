b6206fedb428e23819e0c21219f1d9e8048f7161
Laurent Raufaste <analogue@glop.org>
2020-06-11 21:42:36 -0400

    Try not async

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ options.cwd = '/tmp/';
 
 const version = core.getInput('version');
 
-await exec.exec('curl', [
+exec.exec('curl', [
   '-L',
   '-o /tmp/kubeval-linux-amd64.tar.gz',
   `https://github.com/instrumenta/kubeval/releases/download/${payload}/kubeval-linux-amd64.tar.gz`
 ], options);
-await exec.exec('tar', ['xf', 'kubeval-linux-amd64.tar.gz']);
-await exec.exec('sudo', ['cp', 'kubeval', '/usr/local/bin']);
-await exec.exec('rm', ['kubeval', 'kubeval-linux-amd64.tar.gz']);
+exec.exec('tar', ['xf', 'kubeval-linux-amd64.tar.gz']);
+exec.exec('sudo', ['cp', 'kubeval', '/usr/local/bin']);
+exec.exec('rm', ['kubeval', 'kubeval-linux-amd64.tar.gz']);
